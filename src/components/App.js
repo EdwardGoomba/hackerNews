b092410da1ee8a8930e28e1df34a9266f26bf596
EdwardGoomba <edward@dock90.io>
2019-11-23 17:51:58 -0500

    Setup CreateLink component and associated mutation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/App.css';
+// components
+import CreateLink from './CreateLink'
 import LinkList from './LinkList'
 
 const App = () => {
   return (
-    <LinkList />
+    <React.Fragment>
+      <LinkList />
+      <CreateLink />
+    </React.Fragment>
   );
 }
 
