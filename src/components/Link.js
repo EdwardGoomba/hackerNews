766553ba578bd0b84d89698412ce7cd4524afec5
EdwardGoomba <edward@dock90.io>
2019-12-10 19:26:56 -0500

    Setup link voting

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { AUTH_TOKEN } from '../constants'
+import { timeDifferenceForDate } from '../utils'
 
-const Link = ({ link }) => {
+const Link = ({ index, link }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN)
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
+            +
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{' '}
+          {link.postedBy ?
+            link.postedBy.name
+            :
+            'Unknown'
+          }{' '}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
       </div>
     </div>
   )
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -13,6 +13,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -30,7 +40,7 @@ const LinkList = () => {
 
         return (
           <div>
-            {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            {linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} />)}
           </div>
         )
       }}
